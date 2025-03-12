6381787cfe671b4d16703e151c54888bfa1f29c6
Muhammad Yunus <myunus_info@yahoo.com>
2025-03-13 01:52:42 +0200

    New Fixes Added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,12 @@
+'use client';
+
 import LoginForm from '@/components/modules/auth/login/LoginForm';
 import { Suspense } from 'react';
 
 export default function Login() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
         <LoginForm />
       </Suspense>
     </div>
